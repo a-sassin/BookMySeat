bed1c913b703166e10f5f1512cfd8d487cded36d
Amit Ranjan <67773953+a-sassin@users.noreply.github.com>
2022-02-17 17:37:34 +0530

    Add files via upload

---
+++ UI/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    baseUrl: 'https://bms.gslab.com:4000/api/v1/',
+    socketUrl: 'wss://bms.gslab.com:4000',
+    encryptionKey: 'encryptionkeyforbookmyseatapplicationdesignedbygslab',
+};
---
+++ UI/src/environments/environment.release.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://10.35.38.139:3000/api/v1/',
+    socketUrl: 'wss://10.35.38.139:3000',
+    encryptionKey: 'encryptionkeyforbookmyseatapplicationdesignedbygslab',
+};
